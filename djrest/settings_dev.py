9c914e74c52b9d522349c3785d1e80c2b321f6e5
AdachiTetsuya <1019born@gmail.com>
2022-04-08 11:10:28 +0900

    postgresqlの設定を追記

--- djrest/settings_dev.py
+++ djrest/settings_dev.py
@@ -4,8 +4,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
 
 ATTEND_FRONT_ROOT = 'http://localhost:3000'
+
--- djrest/settings_prod.py
+++ djrest/settings_prod.py
@@ -4,6 +4,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['35.72.215.229']
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'demiattend',
+         'USER': 'adachitetsuya',
+         'PASSWORD': '1019born',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
+
+
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://d19u3oyzhhez9d.cloudfront.net',
