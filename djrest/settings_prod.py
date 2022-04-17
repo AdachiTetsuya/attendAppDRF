52abfe43a9fc163959ae421a8336299f73bc814c
AdachiTetsuya <1019born@gmail.com>
2022-04-17 19:09:35 +0900

    データベース移動

--- djrest/settings_prod.py
+++ djrest/settings_prod.py
@@ -4,16 +4,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['35.72.215.229']
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'demiattend',
+#          'USER': 'adachitetsuya',
+#          'PASSWORD': '1019born',
+#          'HOST': 'localhost',
+#          'PORT': '5432',
+#      }
+# }
+
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'demiattend',
-         'USER': 'adachitetsuya',
-         'PASSWORD': '1019born',
-         'HOST': 'localhost',
-         'PORT': '5432',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 CORS_ORIGIN_WHITELIST = (
